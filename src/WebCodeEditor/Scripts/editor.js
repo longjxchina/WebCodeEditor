1e8bb6dbaa3f0f2af2da0698199ccaf88c0358ff
longjxchina <longjxchina@gmail.com>
2012-10-11 13:36:26 +0800

    new add

---
+++ src/WebCodeEditor/Scripts/editor.js
@@ -0,0 +1,33 @@
+﻿var editor;
+
+// 设置编辑器的高度
+function initEditor() {
+    editor = CodeMirror.fromTextArea(document.getElementById("txtCode"), {
+        //mode: "text/xml",
+        mode: "text/x-csharp",
+        matchBrackets: true,
+        lineNumbers: true,
+        
+        // 高亮显示当前行
+        onCursorActivity: function () {
+            editor.setLineClass(hlLine, null, null);
+            hlLine = editor.setLineClass(editor.getCursor().line, null, "activeline");
+        },
+
+        // 自动闭合标签设置
+        extraKeys: {
+            "'>'": function (cm) { cm.closeTag(cm, '>'); },
+            "'/'": function (cm) { cm.closeTag(cm, '/'); }
+        },
+        wordWrap: true
+    });
+
+    // 高亮显示当前行
+    var hlLine = editor.setLineClass(0, "activeline");
+
+    $(".CodeMirror").css({ "background-color": "white" });
+}
+
+function changeMode(mode) {
+    editor.setOption("mode", mode);
+}
